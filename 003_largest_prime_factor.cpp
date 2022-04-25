cd243c3957138d4ce5241b88e7b84cda45605093
Philand <philand.pincon@free.fr>
2022-04-25 23:50:13 +0200

    ADD : beginning of problem 3 script (Problem 3)

---
+++ 003_largest_prime_factor.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* Problem 3 : Largest Prime Factor                                             *
+*                                                                              *
+* The prime factors of 13195 are 5, 7, 13 and 29.                              *
+*                                                                              *
+* What is the largest prime factor of the number 600851475143 ?                *
+*******************************************************************************/
+
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <vector>
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
