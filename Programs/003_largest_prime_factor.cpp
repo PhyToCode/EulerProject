f5178876b2bdcaa4d0cc2d9357ad9070dfba39bc
Philand <philand.pincon@free.fr>
2022-04-30 14:20:53 +0200

    ADD : first method to solve the problem (Problem 3)

--- Programs/003_largest_prime_factor.cpp
+++ Programs/003_largest_prime_factor.cpp
@@ -11,7 +11,64 @@
 #include <cstdlib>
 #include <vector>
 
+#define TEN_THOUSAND 10000
+
+bool is_prime_factor(long int m)
+{
+    bool result = true;
+
+    for (long int k = 2; k < m; k++)
+    {
+        if (m % k == 0)
+            result = false;
+    }
+
+    return result;
+}
+
 int main (int argc, char *argv[])
 {
+
+    /* Input processing */
+    if (argc != 2)
+    {
+        std::cout << "Usage : ./003_largest_prime_factor n" << std::endl;
+        return -1;
+    }
+
+    const long int n = atol(argv[1]);
+
+    /* Variable declaration */
+    std::vector<long int> prime_factor_vector = {2};
+    std::vector<long int> decomposition;
+    long int value = n;
+    bool pf;
+
+    /* Recovery of the first prime factors */
+    for (long int k = 3; k < TEN_THOUSAND; k++)
+    {
+        pf = is_prime_factor(k);
+        if (pf == true)
+            prime_factor_vector.push_back(k);
+    }
+
+    /* Decomposition of n into prime factors */
+    while (value != 1)
+    {
+        int k = 0;
+        long int pfactor = prime_factor_vector[k];
+        while (value % pfactor != 0)
+        {
+            k++;
+            pfactor = prime_factor_vector[k];
+        }
+        decomposition.push_back(pfactor);
+        value /= pfactor;
+        k = 0;
+    }
+
+    /* Display of results */
+    std::cout << "Result of Problem 3 : " << decomposition.back() << std::endl; 
+
     return 0;
 }
