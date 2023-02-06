e2f1dd7b2af02d8edfa804e2f94dcfb6bcdfb13d
Philand <philand.pincon@free.fr>
2023-02-06 19:45:31 +0100

    ADD : completion of palindrom problem (Problem 4)

--- Programs/004_largest_palindrome_product.cpp
+++ Programs/004_largest_palindrome_product.cpp
@@ -8,19 +8,56 @@
 *******************************************************************************/
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+bool check_palindrome(int nb)
+{
+    int reverse = 0; // reverse of the nb number 
+    int temp = nb;   // temporary value
+    while (temp != 0)
+    {
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+    return (reverse == nb);
+}
 
 int main (int argc, char *argv[])
 {
     /* Input processing */
     if (argc != 2)
     {
-        std::cout << "Usage : ./004_largest_palindrome_product" << std::endl;
+        std::cout << "Usage : ./004_largest_palindrome_product digits" << std::endl;
         return -1;
     }
 
+    int digits = atoi(argv[1]);
+    std::vector<int> palindrom_vector;
+    int product;
+
+    int max_range = pow(10, digits) - 1;
+    int min_range = pow(10, digits - 1) - 1;
+
+    /* We check if the numbers are palindromes */
+    for (int k = max_range; k > min_range; k--)
+    {
+        for (int i = max_range; i > min_range; i--)
+        {
+            product = k*i;
+            if (check_palindrome(product) == 1)
+            {
+                palindrom_vector.push_back(product);
+            }
+        }
+    }
+
+    int max_palindrom = *std::max_element(palindrom_vector.begin(), palindrom_vector.end());
+
     /* Display of results */
     std::cout << "Result of Problem 4 : " << std::endl;
-    std::cout << "The largest palindrome product is " << std::endl;
+    std::cout << "The largest palindrome product of two " << digits << "-digit number is " << max_palindrom << std::endl;
 
     return 0;
 }
