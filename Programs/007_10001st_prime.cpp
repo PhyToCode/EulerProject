2f607ad7e068727896dc833e423513f7298ab0eb
Philand <philand.pincon@free.fr>
2023-02-15 17:13:29 +0100

    ADD : solution to compute the n_st prime number (Problem 7)

--- Programs/007_10001st_prime.cpp
+++ Programs/007_10001st_prime.cpp
@@ -7,19 +7,48 @@
 * What is the 10 001st prime number?                                           *
 *******************************************************************************/
 
-#include <iostream>
+#include<iostream>
+#include<vector>
+
+bool isPrimeFactor(long int m)
+{
+    bool result = true;
+
+    for (long int k = 2; k < m; k++)
+    {
+        if (m % k == 0)
+            result = false;
+    }
+
+    return result;
+}
 
 int main (int argc, char *argv[])
 {
     /* Input processing */
-    if (argc != 1)
+    if (argc != 2)
     {
-        std::cerr << "Usage : ./007_10001st_prime" << std::endl;
+        std::cerr << "Usage : ./007_10001st_prime n" << std::endl;
         return -1;
     }
 
+    const long int n = atol(argv[1]);
+
+    std::vector<long int> prime_factor_vector = {2};
+    bool pf;
+    long int k = 3;
+
+    /* Recovery of the first prime factors */
+    while (prime_factor_vector.size() < n)
+    {
+        pf = isPrimeFactor(k);
+        if (pf == true)
+            prime_factor_vector.push_back(k);
+        k++;
+    }
+
     /* Display of results */
-    std::cout << "Result of Problem 7 : " << std::endl;
+    std::cout << "Result of Problem 7 : " << prime_factor_vector.back() << std::endl;
 
     return 0;
 }
